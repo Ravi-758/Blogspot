fbfc4be64534c6e9260544039117ecac73c0c742
JassSangha <nirbhau.jass@gmail.com>
2024-03-28 13:32:54 +0530

    5

--- components/mail.php
+++ components/mail.php
@@ -18,7 +18,7 @@ function sendMail($email, $name, $token){
         $mail->isSMTP();                        
         $mail->Host       = 'smtp-relay.brevo.com'; 
         $mail->SMTPAuth   = true;        
-        $mail->Username   = '';    
+        $mail->Username   = 'sanjnadadralh1970@gmail.com';    
         $mail->Password   = '';
         $mail->SMTPSecure = 'tls';              
         $mail->Port       = 587;                                  
